24745608fb3058c2b08b9f02234e2c385670a7f8
spouk <spouk@spouk.ru>
2020-02-04 20:20:23 +0300

    render add io.writer  executetemplate

--- render/render.go
+++ render/render.go
@@ -167,6 +167,28 @@ func (s *Render) ReloadTemplate() {
 }
 
 //перегружает отдельный блок/шаблон для обновления данных
+func (s *Render) ExecuteTemplateIO(name string, data interface{}, w io.Writer) {
+	if err := s.Temp.ExecuteTemplate(w, name, data); err != nil {
+		if s.logwriterEnable {
+			_, _ = s.Lg.Write([]byte(fmt.Sprintf(ERROR_EXECUTETEMPLATE, err.Error())))
+		} else {
+			s.logger.Printf(fmt.Sprintf(ERROR_EXECUTETEMPLATE, err.Error()))
+		}
+	}
+}
+
+//перегружает отдельный блок/шаблон для обновления данных
+func (s *Render) ExecuteTemplateIO(name string, data interface{}, w io.Writer) {
+	if err := s.Temp.ExecuteTemplate(w, name, data); err != nil {
+		if s.logwriterEnable {
+			_, _ = s.Lg.Write([]byte(fmt.Sprintf(ERROR_EXECUTETEMPLATE, err.Error())))
+		} else {
+			s.logger.Printf(fmt.Sprintf(ERROR_EXECUTETEMPLATE, err.Error()))
+		}
+	}
+}
+
+//перегружает отдельный блок/шаблон для обновления данных
 func (s *Render) ExecuteTemplate(name string, data interface{}, w http.ResponseWriter) {
 	if err := s.Temp.ExecuteTemplate(w, name, data); err != nil {
 		if s.logwriterEnable {
