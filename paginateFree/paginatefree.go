06cfa85b0d25ca62cc31e34fabb95db1d0baf6dc
spouk <spouk@spouk.ru>
2020-05-16 22:36:39 +0300

    add paginator free

--- paginateFree/paginatefree.go
+++ paginateFree/paginatefree.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"log"
 	"os"
+	"reflect"
 )
 
 const (
@@ -13,6 +14,7 @@ const (
 
 var (
 	errorPage = errors.New("not correct number current page")
+	errorType = errors.New("error: type is not slice")
 )
 
 type Paginator struct {
@@ -22,8 +24,8 @@ type PaginateResult struct {
 	Page       int             //текущая страница
 	TotalPage  int             //всего страниц
 	CountPage  int             //количество элементов на странице
-	List       []interface{}   //общий список
-	Block      []interface{}   //индекс блока=страницы
+	List       interface{}     //общий список
+	Block      interface{}     //индекс блока=страницы
 	TotalBlock [][]interface{} //список всех блоков
 }
 
@@ -33,26 +35,39 @@ func NewPaginator() *Paginator {
 	}
 	return &p
 }
-func (p *Paginator) Paginate(page, countPage int, list []interface{}) (*PaginateResult, error) {
+func (p *Paginator) Paginate(page, countPage int, list interface{}) (*PaginateResult, error) {
 	var pr PaginateResult
+	var tt = reflect.TypeOf(list)
+	var vv = reflect.ValueOf(list)
+
+	//check slice type
+	if tt.Kind() != reflect.Slice {
+		return nil, errorType
+	}
+	//make list interface for export
+	var res = make([]interface{}, vv.Len())
+	for i := 0; i < vv.Len(); i++ {
+		res[i] = vv.Index(i).Interface()
+	}
+
 	// totalRecords <= countPage
-	if len(list) == 0 || len(list) <= countPage {
+	if vv.Len() == 0 || vv.Len() <= countPage {
 		pr.Page = page
 		pr.TotalPage = 1
 		pr.CountPage = countPage
 		pr.List = list
 		pr.Block = list
 		pr.TotalBlock = make([][]interface{}, 1)
-		pr.TotalBlock[0] = list
+		pr.TotalBlock[0] = res
 		return &pr, nil
 	}
 
 	// calculate totalpage
-	yes := len(list) % countPage
+	yes := vv.Len() % countPage
 	if yes > 0 {
-		pr.TotalPage = (len(list) / countPage) + 1
+		pr.TotalPage = (vv.Len() / countPage) + 1
 	} else {
-		pr.TotalPage = len(list) / countPage
+		pr.TotalPage = vv.Len() / countPage
 	}
 
 	//check correct number page
@@ -66,11 +81,11 @@ func (p *Paginator) Paginate(page, countPage int, list []interface{}) (*Paginate
 	for x := 0; x < pr.TotalPage; x++ {
 		start = x * countPage
 		if x == pr.TotalPage-1 {
-			step = len(list)
+			step = vv.Len()
 		} else {
 			step = start + countPage
 		}
-		result[x] = list[start:step]
+		result[x] = res[start:step]
 	}
 
 	//return result
